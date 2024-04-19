2d6241c745532a6dac3caa06483a84c969eb7a7e
Rhysypops <developer@meridiantrust.co.uk>
2024-04-19 12:45:10 +0100

    Update SiteHeader.tsx

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -18,32 +18,34 @@ export default function SiteHeader({ header }: { header: any }) {
   return (
     <header className="fixed z-20 w-full">
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-9xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+        <div className="hidden lg:flex">
+          <Link href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Stratton School</span>
             <Image
               src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${header.logo.url}`}
               width={header.logo.width}
               height={header.logo.height}
               alt={header.logo.alt}
-              className="h-20 w-auto"
+              className="resize-none max-w-60"
             />
-          </a>
+          </Link>
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Menu className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+        {!mobileMenuOpen && (
+          <div className="flex w-full justify-end lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Menu className="h-10 w-10" aria-hidden="true" />
+            </button>
+          </div>
+        )}
+        <Popover.Group className="hidden lg:flex flex-wrap justify-end lg:gap-x-12">
           {primaryNavItems.length > 0 &&
             primaryNavItems.map((item: any) => (
               <Popover
@@ -94,66 +96,74 @@ export default function SiteHeader({ header }: { header: any }) {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
+            <Link href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Stratton School</span>
+              <Image
+                src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${header.logo.url}`}
+                width={header.logo.width}
+                height={header.logo.height}
+                alt={header.logo.alt}
+                className="resize-none max-w-60"
               />
-            </a>
+            </Link>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 rounded-md p-2.5 text-white"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
-              <X className="h-6 w-6" aria-hidden="true" />
+              <X className="h-10 w-10" aria-hidden="true" />
             </button>
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <Disclosure as="div" className="-mx-3">
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                        Company
-                        <ChevronDown
-                          className={classNames(
-                            open ? "rotate-180" : "",
-                            "h-5 w-5 flex-none"
+              {primaryNavItems.length > 0 &&
+                primaryNavItems.map((item: any) => (
+                  <div className="space-y-2 py-6">
+                    {item.children.length > 0 ? (
+                      item.children.map((child: any) => (
+                        <Disclosure as="div" className="-mx-3">
+                          {({ open }) => (
+                            <>
+                              <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-white transition-all hover:bg-neutral-900">
+                                {item.label}
+                                <ChevronDown
+                                  className={classNames(
+                                    open ? "rotate-180" : "",
+                                    "h-5 w-5 flex-none"
+                                  )}
+                                  aria-hidden="true"
+                                />
+                              </Disclosure.Button>
+                              <Disclosure.Panel className="mt-2 space-y-2">
+                                {primaryNavItems.length > 0 &&
+                                  primaryNavItems.map((item: any) => (
+                                    <Disclosure.Button
+                                      key={item.name}
+                                      as="a"
+                                      href={item.href}
+                                      className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    >
+                                      {item.name}
+                                    </Disclosure.Button>
+                                  ))}
+                              </Disclosure.Panel>
+                            </>
                           )}
-                          aria-hidden="true"
-                        />
-                      </Disclosure.Button>
-                      <Disclosure.Panel className="mt-2 space-y-2">
-                        {primaryNavItems.length > 0 &&
-                          primaryNavItems.map((item: any) => (
-                            <Disclosure.Button
-                              key={item.name}
-                              as="a"
-                              href={item.href}
-                              className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                            >
-                              {item.name}
-                            </Disclosure.Button>
-                          ))}
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure>
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </div>
+                        </Disclosure>
+                      ))
+                    ) : (
+                      <a
+                        href="#"
+                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white transition-all hover:bg-neutral-900"
+                      >
+                        Log in
+                      </a>
+                    )}
+                  </div>
+                ))}
             </div>
           </div>
         </Dialog.Panel>
