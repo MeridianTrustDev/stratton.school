a977f3e5938ceef786c46c90a195e77e8ec10f0a
Rhysypops <developer@meridiantrust.co.uk>
2024-04-22 15:45:05 +0100

    1

---
+++ src/app/sitemap.ts
@@ -0,0 +1,37 @@
+import { MetadataRoute } from "next";
+import qs from "qs";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  try {
+    const query = {
+      "tenant.name": {
+        equals: "Stratton School",
+      },
+    };
+
+    const stringifiedQuery = qs.stringify(
+      {
+        where: query,
+      },
+      { addQueryPrefix: true }
+    );
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/pages${stringifiedQuery}`
+    );
+
+    const { docs } = await response.json();
+
+    const pageEntries: MetadataRoute.Sitemap = docs.map(
+      ({ slug, updatedAt }: { slug: string; updatedAt: string }) => ({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/${slug}`,
+        lastModified: new Date(updatedAt),
+      })
+    );
+
+    return pageEntries;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
