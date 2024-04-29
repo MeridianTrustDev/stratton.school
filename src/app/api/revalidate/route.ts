ed43b14cf47d62ccbbd93c622632be6fca4b1503
Rhysypops <developer@meridiantrust.co.uk>
2024-04-29 09:00:17 +0100

    Create route.ts

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,32 @@
+import { revalidatePath, revalidateTag } from "next/cache";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const path = request.nextUrl.searchParams.get("path");
+  const tag = request.nextUrl.searchParams.get("tag");
+  const secret = request.nextUrl.searchParams.get("secret");
+
+  if (secret !== process.env.REVALIDATION_KEY) {
+    return Response.json({
+      revalidated: false,
+      now: Date.now(),
+      message: "Invalid secret",
+    });
+  }
+
+  if (path) {
+    revalidatePath(path);
+    return Response.json({ revalidated: true, now: Date.now() });
+  }
+
+  if (tag) {
+    revalidateTag(tag);
+    return Response.json({ revalidated: true, now: Date.now() });
+  }
+
+  return Response.json({
+    revalidated: false,
+    now: Date.now(),
+    message: "Missing path to revalidate",
+  });
+}
