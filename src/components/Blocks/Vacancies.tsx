8b7a883e641f21c3642db05d67efdc7e2c6457eb
Rhysypops <developer@meridiantrust.co.uk>
2024-06-13 15:52:41 +0100

    Update Vacancies.tsx

--- src/components/Blocks/Vacancies.tsx
+++ src/components/Blocks/Vacancies.tsx
@@ -1,27 +1,38 @@
 "use client";
 
 import Script from "next/script";
+import { useEffect } from "react";
 
 export default function Vacancies({ myNewTermID }: { myNewTermID: string }) {
-  return (
-    <>
-      <Script
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `var mntInitCounter = 0, mntApiScript = document.createElement("script");
-  mntApiScript.type = "text/javascript";
-  mntApiScript.src = "https://api.mynewterm.com/assets/v1/dist/js/school_vacancies.js?v=" + (new Date().getTime());
-  document.body.appendChild(mntApiScript);
+  useEffect(() => {
+    let mntInitCounter = 0;
 
-  window.onload = function () {
-    if(document.readyState == 'complete' && mntInitCounter === 0) {
+    const mntApiScript = document.createElement("script");
+    mntApiScript.type = "text/javascript";
+    mntApiScript.src =
+      "https://api.mynewterm.com/assets/v1/dist/js/school_vacancies.js?v=" +
+      new Date().getTime();
+
+    mntApiScript.onload = () => {
+      if (mntInitCounter === 0) {
         mntInitCounter = 1;
-        mntSchoolVacancies('${myNewTermID}', 1);
-    }
-  };`,
-        }}
-      />
-      <div id="mnt-parent-container" className="w-[96.7%] bg-transparent"></div>
-    </>
+        // @ts-ignore
+        if (typeof mntSchoolVacancies === "function") {
+          // @ts-ignore
+          mntSchoolVacancies(myNewTermID, 1);
+        } else {
+          console.error("mntSchoolVacancies function is not available");
+        }
+      }
+    };
+
+    document.body.appendChild(mntApiScript);
+
+    return () => {
+      document.body.removeChild(mntApiScript);
+    };
+  }, []);
+  return (
+    <div id="mnt-parent-container" className="w-[96.7%] bg-transparent"></div>
   );
 }
