4d1c3a44e0a615ef2ab188baacd7d20eacae784b
Rhysypops <developer@meridiantrust.co.uk>
2024-07-16 11:47:17 +0100

    Update header.ts

--- src/lib/payload/header.ts
+++ src/lib/payload/header.ts
@@ -16,7 +16,7 @@ export const getHeader = async () => {
     );
 
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/headers${stringifiedQuery}&depth=4`,
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/headers${stringifiedQuery}&select[0]=title&select[1]=title&select[2]=logo&select[3]=primaryNavigation.type&select[4]=primaryNavigation.navItems.reference.title&select[5]=primaryNavigation.navItems.reference.breadcrumbs`,
       {
         next: {
           tags: ["navigation"],
